3f3e92183eecca944499b6d705cb810b6f1e4110
Minji Kim <kimminji080122@gmail.com>
2022-03-09 12:32:18 +0900

    Create 평균 구하기.py

---
+++ Programmers Lv.1/평균 구하기.py
@@ -0,0 +1,11 @@
+# 연습문제 > 평균 구하기
+# 링크: https://programmers.co.kr/learn/courses/30/lessons/12944
+
+def solution(arr):
+    return sum(arr) / len(arr)
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(solution([1, 2, 3, 4]))    # 2.5
+print(solution([5, 5]))    # 5
+
+# 문제 풀이 시간: 2분
