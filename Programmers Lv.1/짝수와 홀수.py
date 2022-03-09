9a4a80008df70328fcc067d157deb291792f6728
Minji Kim <kimminji080122@gmail.com>
2022-03-09 17:22:00 +0900

    Create 짝수와 홀수.py

---
+++ Programmers Lv.1/짝수와 홀수.py
@@ -0,0 +1,11 @@
+# 연습문제 > 짝수와 홀수
+# 링크: https://programmers.co.kr/learn/courses/30/lessons/12937?language=python3
+
+def solution(num):
+    return "Even" if num % 2 == 0 else "Odd"
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(solution(3))    # "Odd"
+print(solution(4))    # "Even"
+
+# 문제 풀이 시간: 1분
