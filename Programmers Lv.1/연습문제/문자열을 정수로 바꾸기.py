2dab24c19d4a1088565c1e72ea528fa1417fa0de
Minji Kim <kimminji080122@gmail.com>
2022-03-10 21:41:29 +0900

    Create 문자열을 정수로 바꾸기.py

---
+++ Programmers Lv.1/연습문제/문자열을 정수로 바꾸기.py
@@ -0,0 +1,11 @@
+# 연습문제 > 문자열을 정수로 바꾸기
+# 링크: https://programmers.co.kr/learn/courses/30/lessons/12925?language=python3
+
+def solution(s):
+    return int(s)
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(solution("1234"))    # 1234
+print(solution("-1234"))    # -1234
+
+# 문제 풀이 시간: 1분
