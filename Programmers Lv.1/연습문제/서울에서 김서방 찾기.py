81707f93b8469e157ba84b26d4b696521c707145
Minji Kim <kimminji080122@gmail.com>
2022-03-10 21:42:24 +0900

    Create 서울에서 김서방 찾기.py

---
+++ Programmers Lv.1/연습문제/서울에서 김서방 찾기.py
@@ -0,0 +1,10 @@
+# 연습문제 > 서울에서 김서방 찾기
+# 링크: https://programmers.co.kr/learn/courses/30/lessons/12919?language=python3
+
+def solution(seoul):
+    return "김서방은 " + str(seoul.index("Kim")) + "에 있다"
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(solution(["Jane", "Kim"]))    # "김서방은 1에 있다"
+
+# 문제 풀이 시간: 3분
