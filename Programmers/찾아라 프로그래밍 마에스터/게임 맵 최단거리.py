1b1bf70f07b35f57a11a966e6de3d00d4d0cf69b
Minji Kim <kimminji080122@gmail.com>
2022-04-11 20:23:18 +0900

    Create 게임 맵 최단거리.py

---
+++ Programmers/찾아라 프로그래밍 마에스터/게임 맵 최단거리.py
@@ -0,0 +1,45 @@
+# 찾아라 프로그래밍 마에스터 > 게임 맵 최단거리
+# 링크: https://programmers.co.kr/learn/courses/30/lessons/1844
+
+from collections import deque
+def solution(maps):
+    answer = 0
+
+    # 상하좌우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    def bfs(x, y):
+        queue = deque()
+        queue.append((x, y))
+
+        # queue가 빌 때까지 반복
+        while queue:
+            x, y = queue.popleft()
+
+            # 상하좌우 칸 확인하기
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                # 맵을 벗어나면 무시하기
+                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]): continue
+
+                # 벽이면 무시하기
+                if maps[nx][ny] == 0:  continue
+
+                # 처음 지나가는 길이면 거리계산하고 다시 상하좌우 확인하기
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx, ny))    # 재귀
+
+        # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리
+        return maps[len(maps)-1][len(maps[0])-1]
+
+    answer = bfs(0, 0)
+    return -1 if answer == 1 else answer    # 상대 팀 진영에 도착할 수 없을 때 -1
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))    # 11
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))    # -1
+
+# 문제 풀이 시간: 36분
