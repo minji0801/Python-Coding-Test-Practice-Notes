6e821574c3a98f80d34716e88e1371ec685e1a91
Minji Kim <kimminji080122@gmail.com>
2022-04-12 21:50:05 +0900

    Create 1268 최댓값, 최솟값.py

---
+++ CodeUp/관리자 추천 문제/1268 최댓값, 최솟값.py
@@ -0,0 +1,12 @@
+# 링크:https://codeup.kr/problem.php?id=1286
+
+# nums = [3,7,-4,-6,5]
+
+nums = []
+for _ in range(5):
+    nums.append(int(input()))
+
+print(max(nums))
+print(min(nums))
+
+# 문제 풀이 시간: 2분
